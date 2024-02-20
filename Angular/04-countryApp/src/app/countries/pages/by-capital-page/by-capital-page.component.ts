93201ea2f6c9c4ff219971f4dd2b32a1820d6826
Emmanuel Gomez <gomezgtzemmanuel@gmail.com>
2024-02-19 18:49:51 -0600

    Country SPA

---
+++ Angular/04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'by-capital-page',
+  templateUrl: './by-capital-page.component.html',
+  styles: [],
+})
+export class ByCapitalPageComponent {
+  public countries: Country[] = [];
+
+  constructor(private countriesService: CountriesService) {}
+
+  searchByCapital(term: string) {
+    this.countriesService.searchCapital(term).subscribe((countries) => {
+      this.countries = countries;
+    });
+    console.log('From Capital Page');
+    console.log({ term });
+  }
+}
