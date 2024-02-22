2244f82ea3f419c59a55bc38a960859464efdae0
Emmanuel Gomez <gomezgtzemmanuel@gmail.com>
2024-02-21 21:41:23 -0600

    Angular Material

--- Angular/06-heroesApp/src/app/app.module.ts
+++ Angular/06-heroesApp/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
+    HttpClientModule,
+
+    AppRoutingModule,
     SharedModule,
   ],
   providers: [],
---
+++ Angular/06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environments } from 'src/environments/environments';
+import { Hero } from '../interfaces/hero.interface';
+import { Observable, catchError, of } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class HeroesService {
+  private baseUrl: string = environments.baseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.http
+      .get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(catchError(() => of(undefined)));
+  }
+  getSuggestions( query: string ) : Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${ query }&_limit=20`);
+  }
+}
